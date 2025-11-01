008951c5189ec84dd0a0f32cf4ff7cbb309b7244
cxxzhang <cxxzhang@gmail.com>
2025-11-01 09:03:32 +0800

    build:add openssl-1.1.1w static library binary with /MT /MTd build

--- onekey-build-x64.bat
+++ onekey-build-x64.bat
@@ -20,11 +20,31 @@ set InstallDir="%~dp0Output\x64"
 
 mkdir %InstallDir%
 
+set "OpenSSLRoot=%~dp0openssl"
+set "OpenSSLInclude=%OpenSSLRoot%\include"
+set "OpenSSLLibRelease=%OpenSSLRoot%\lib\x64"
+set "OpenSSLLibDebug=%OpenSSLRoot%\lib\x64d"
+
+if not exist "%OpenSSLLibRelease%\libssl.lib" (
+  echo OpenSSL release libraries not found in %OpenSSLLibRelease%
+  exit /B 1
+)
+if not exist "%OpenSSLLibDebug%\libssl.lib" (
+  echo OpenSSL debug libraries not found in %OpenSSLLibDebug%
+  exit /B 1
+)
+
+set "OPENSSL_INCDIR=%OpenSSLInclude%"
+set "OPENSSL_LIBDIR=%OpenSSLLibRelease%"
+set "OPENSSL_LIBS=/LIBPATH:%OpenSSLLibRelease% libssl.lib libcrypto.lib Crypt32.lib User32.lib Ws2_32.lib Gdi32.lib Advapi32.lib"
+set "OPENSSL_LIBS_RELEASE=%OPENSSL_LIBS%"
+set "OPENSSL_LIBS_DEBUG=/LIBPATH:%OpenSSLLibDebug% libssl.lib libcrypto.lib Crypt32.lib User32.lib Ws2_32.lib Gdi32.lib Advapi32.lib"
+
 set PATH=%~dp0qtbase\bin;%PATH%
 
 cd /d "%~dp0"
 
-call configure -prefix %InstallDir% -confirm-license -opensource -debug-and-release -force-debug-info -opengl dynamic -no-directwrite -mp -nomake examples -nomake tests -recheck-all
+call configure -prefix %InstallDir% -confirm-license -opensource -debug-and-release -force-debug-info -opengl dynamic -no-directwrite -mp -nomake examples -nomake tests -recheck-all -openssl-linked -I "%OpenSSLInclude%" -L "%OpenSSLLibRelease%"
 nmake
 
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
